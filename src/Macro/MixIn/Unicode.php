1a51477af9aad26267e7a8d850520aefa78f92e2
nabetks <nabetks@users.noreply.github.com>
2024-10-29 12:55:09 +0000

    Fix styling

--- src/Macro/MixIn/Unicode.php
+++ src/Macro/MixIn/Unicode.php
@@ -10,7 +10,8 @@ class Unicode
 
     /**
      * 文章の前後の空白を削除する。
-     * @param string|array $value 前後の空白を削除する文字列又は配列
+     *
+     * @param  string|array  $value  前後の空白を削除する文字列又は配列
      * @return string|array 前後の空白を削除した文字列又は配列
      */
     public function trimSpace(string|array $value): string|array
