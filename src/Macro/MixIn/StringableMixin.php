4ce1caae2df72b281621b1fa44d6d0197970fa13
渡部 隆史 <nabe@ai-joh.co.jp>
2024-10-30 05:53:29 +0900

    PHPStanのエラー対応 private static のアクセスを static => selfに変更 StringableMixin $value プロパティ追加

--- src/Macro/MixIn/StringableMixin.php
+++ src/Macro/MixIn/StringableMixin.php
@@ -7,7 +7,9 @@ use Illuminate\Support\Stringable;
 
 class StringableMixin
 {
-    private string $value;
+    public function __construct(private string $value)
+    {
+    }
 
     /**
      * ひらがな、カタカナ、半角カタカナを全角カタカナに変換
