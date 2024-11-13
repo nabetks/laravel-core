24e48baf041871d8e96080270abe44be11b55f43
nabetks <nabetks@users.noreply.github.com>
2024-11-13 19:10:09 +0000

    Fix styling

--- src/Rules/Base/RuleBase.php
+++ src/Rules/Base/RuleBase.php
@@ -6,28 +6,22 @@ use Aijoh\Core\Rules\Trait\InnerValidator;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Contracts\Validation\ValidatorAwareRule;
-use Illuminate\Validation\Validator;
 
 /**
  * Class RuleBase
- * @package Aijoh\Core\Rules\Base
  */
-class RuleBase implements ValidationRule, ValidatorAwareRule {
-
+class RuleBase implements ValidationRule, ValidatorAwareRule
+{
     use InnerValidator;
-
 
     /**
      * バリデーションルールを設定する。
-     * @param string $attribute
-     * @param mixed $value
-     * @param Closure $fail
-     * @return void
      */
-    public final function validate( string $attribute, mixed $value, Closure $fail ) : void {
+    final public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
         $innerRules = $this->getInnerRules();
-        if( ! empty($innerRules) ){
-            if( ! $this->validation($attribute, $value, $fail, $innerRules, $this->getInnerMessages()) ){
+        if (! empty($innerRules)) {
+            if (! $this->validation($attribute, $value, $fail, $innerRules, $this->getInnerMessages())) {
                 return;
             }
         }
@@ -36,28 +30,24 @@ class RuleBase implements ValidationRule, ValidatorAwareRule {
 
     /**
      * バリデーションルールを設定する。
+     *
      * @return array
      */
-    protected function getInnerRules() : array|string {
-        return "";
+    protected function getInnerRules(): array|string
+    {
+        return '';
     }
 
     /**
      * バリデーションメッセージを設定する。
-     * @return array
      */
-    protected function getInnerMessages() : array {
+    protected function getInnerMessages(): array
+    {
         return [];
     }
 
-
     /**
      * 独自ルールを設定する。
-     * @param string $attribute
-     * @param mixed $value
-     * @param Closure $fail
-     * @return void
      */
-    protected function customRule(string $attribute, mixed $value, Closure $fail) : void {
-    }
+    protected function customRule(string $attribute, mixed $value, Closure $fail): void {}
 }
