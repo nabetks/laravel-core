577b97cfbcc787a907ff410d698bade054187dd1
nabetks <nabetks@users.noreply.github.com>
2024-11-23 03:11:01 +0000

    Fix styling

--- src/ValueObjects/Boolean.php
+++ src/ValueObjects/Boolean.php
@@ -4,9 +4,8 @@ namespace Aijoh\Core\ValueObjects;
 
 use Illuminate\Support\Str;
 
-
-class Boolean extends BaseObject {
-
+class Boolean extends BaseObject
+{
     protected static $falseList = [
         'false',
         '0',
@@ -20,64 +19,60 @@ class Boolean extends BaseObject {
         false,
     ];
 
-
-    public static function beforeValidate( mixed $value ) : mixed {
-        if ( is_bool($value) ) {
-            return (bool)$value;
+    public static function beforeValidate(mixed $value): mixed
+    {
+        if (is_bool($value)) {
+            return (bool) $value;
         }
 
-        if ( is_null($value) ) {
+        if (is_null($value)) {
             return false;
         }
 
-
-        if ( is_int($value) ) {
-            return (bool)$value;
+        if (is_int($value)) {
+            return (bool) $value;
         }
 
-        if ( is_string($value) ) {
-            $format = (string)Str::of($value)->trimSpace()->lower();
+        if (is_string($value)) {
+            $format = (string) Str::of($value)->trimSpace()->lower();
+
             return static::isFalseString($format) ? false : true;
         }
 
         logger()->info("boolean 判別の不明な型です: $value");
+
         return false;
     }
 
     /**
      * 文字列がfalseに該当するかをチェックする。
-     * @param string $value
-     * @return bool
      */
-    protected static function isFalseString( string $value ) : bool {
+    protected static function isFalseString(string $value): bool
+    {
         return in_array($value, static::$falseList);
     }
 
-
     /**
      * バリデーション前のデータ変更を行う
-     * @param mixed $value
-     * @return mixed
      */
-    public function formatValue( mixed $value ) : mixed {
+    public function formatValue(mixed $value): mixed
+    {
         return static::beforeValidate($value);
     }
 
     /**
      * 値を取得する
-     * @return mixed
      */
-    public function value() : mixed {
-        return (bool)$this->value;
+    public function value(): mixed
+    {
+        return (bool) $this->value;
     }
-
 
     /**
      * 文字列を取得する
-     * @return string
      */
-    public function __toString() : string {
+    public function __toString(): string
+    {
         return $this->value() ? 'true' : 'false';
     }
 }
-
