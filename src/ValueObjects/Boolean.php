9c7cbe1847e7ff79c7656330d6962a84afba4ac9
nabetks <nabetks@users.noreply.github.com>
2024-11-23 02:36:27 +0000

    Fix styling

--- src/ValueObjects/Boolean.php
+++ src/ValueObjects/Boolean.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace Aijoh\Core\ValueObjects;
+
 use Illuminate\Support\Facades\Str;
 
-
-class Boolean extends BaseObject {
-
+class Boolean extends BaseObject
+{
     protected static $falseList = [
         'false',
         '0',
@@ -19,26 +19,25 @@ class Boolean extends BaseObject {
         false,
     ];
 
-
-    public static function beforeValidate( mixed $value ) : mixed {
-        if( is_string($value) ) {
-            return (string)Str::of($value)->trimSpace()->lower();
+    public static function beforeValidate(mixed $value): mixed
+    {
+        if (is_string($value)) {
+            return (string) Str::of($value)->trimSpace()->lower();
         }
+
         return $value;
     }
 
-
     /**
      * バリデーション前のデータ変更を行う
-     * @param mixed $value
-     * @return mixed
      */
-    public function formatValue( mixed $value ) : mixed {
-        return match($value) {
+    public function formatValue(mixed $value): mixed
+    {
+        return match ($value) {
             is_bool($value) => $value,
             is_null($value) => false,
             is_float($value) => $value !== 0.0,
-            is_int($value) || is_numeric($value) => (int)$value !== 0,
+            is_int($value) || is_numeric($value) => (int) $value !== 0,
             is_string($value) => Str::of($value)->trimSpace()->lower()->in(static::$falseList),
             default => false,
         };
@@ -46,19 +45,17 @@ class Boolean extends BaseObject {
 
     /**
      * 値を取得する
-     * @return mixed
      */
-    public function value() : mixed {
-        return (bool)$this->value;
+    public function value(): mixed
+    {
+        return (bool) $this->value;
     }
-
 
     /**
      * 文字列を取得する
-     * @return string
      */
-    public function __toString() : string {
+    public function __toString(): string
+    {
         return $this->value() ? 'true' : 'false';
     }
 }
-
